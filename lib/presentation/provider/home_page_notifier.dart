c957fbe6a3f27b536f2f6f1ecd235732e8cbd0b8
Kai Koga <koga.kai@icloud.com>
2022-07-01 19:55:32 +0800

    Refactory state management code

--- lib/presentation/provider/home_page_notifier.dart
+++ lib/presentation/provider/home_page_notifier.dart
@@ -3,25 +3,59 @@ import 'package:bukit_vista_flutter_assessment/domain/entities/guess.dart';
 import 'package:bukit_vista_flutter_assessment/domain/use_case/get_guess.dart';
 import 'package:flutter/material.dart';
 
+import '../../domain/entities/booking.dart';
+
 class HomePageNotifier extends ChangeNotifier {
   HomePageNotifier({required this.getGuess});
 
-  // This is the state of the request.
+  /*
+    This is state of guess being selected by user in home page.
+    The default value are dummy guess data.
+  */
+  Guess _selectedGuess = Guess(
+    name: "name",
+    imgUrl:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    location: "location",
+    email: "email",
+    phoneNumber: 9373772,
+    guessSince: DateTime.now(),
+    booking: [
+      Booking(
+        id: "id",
+        placeName: "placeName",
+        status: "Confirmed",
+        checkIn: DateTime.now(),
+        checkOut: DateTime.now(),
+      )
+    ],
+  );
+
+  // This is getter for selected guess data.
+  Guess get selectedGuess => _selectedGuess;
+  // This is setter for selected guess data.
+  set selectedGuess(Guess value) {
+    _selectedGuess = value;
+    notifyListeners();
+  }
+
+  // This is the state of the guess list
   List<Guess> _guess = [];
+  // This is getter for guess list.
   List<Guess> get guess => _guess;
 
+  // This is the state of the request.
   RequestState _requestState = RequestState.none;
+  // This is getter for request state.
   RequestState get requestState => _requestState;
 
-  //
+  //This is the information that is returned from the request when error happen.
   String _message = '';
   String get message => _message;
 
   final GetGuess getGuess;
 
-  /*
-    This is a method to get the guess list.
-  */
+  // This is a method to fetch the guess list.
   Future<void> getGuessList() async {
     /*
       Set state to loading and notify the listeners.
